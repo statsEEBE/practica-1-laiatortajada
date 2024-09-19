a285e248e5fe4437c414796c18463e544b6f5fe0
laia.tortajada <laia.tortajada@auleseebe.upc.edu>
2024-09-19 11:52:37 +0200

    ...

--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,17 @@
 #Codigo para problema 3
+x <- c(-8,5,2,-8,9,5,2,-3,1,-1,4-4,9,3,-9,7,0,-7,8,-4,1,7,-6,4,5,-9,-2,-8,5,-5)
+sum(x)
+#per seleccionar una posició
+x[15]
+x[15:20]
+#eliminar una posició
+
+y<- x[-c(21,2,26)]
+
+sum(exp(x))
+sum(exp(y))
+sum(exp(x))-sum(exp(y))
+
+
+y[19]
+y[24]
